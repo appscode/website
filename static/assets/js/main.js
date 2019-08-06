0d44cd7d2e65b558e2421e8717d70aec56e42230
mohincse <mohin@appscode.com>
2019-08-06 19:17:17 +0600

    bottom to top button add in home page and if we need we can use another page

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -211,3 +211,31 @@ function fixNav() {
 }
 
 window.addEventListener("scroll", fixNav);
+
+// scroll to top
+var basicScrollTop = function() {
+  // The button
+  var btnTop = document.querySelector("#goTop");
+  // Reveal the button
+  var btnReveal = function() {
+    if (window.scrollY >= 300) {
+      btnTop.classList.add("is-visible");
+    } else {
+      btnTop.classList.remove("is-visible");
+    }
+  };
+  // Smooth scroll top
+  var TopscrollTo = function() {
+    if (window.scrollY != 0) {
+      setTimeout(function() {
+        window.scrollTo(0, window.scrollY - 30);
+        TopscrollTo();
+      }, 5);
+    }
+  };
+  // Listeners
+  window.addEventListener("scroll", btnReveal);
+  btnTop.addEventListener("click", TopscrollTo);
+};
+basicScrollTop();
+
