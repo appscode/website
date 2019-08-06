ca95263ef82b2dfd43af226e3110e48035975f79
mohincse <mohin@appscode.com>
2019-08-06 17:37:02 +0600

    Header area fixed for responsive design, Appscode home page and inner-page responsive design and fix other

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -194,15 +194,15 @@ tabItems.forEach(tab => {
   });
 });
 
+
 // menu sticky
 // Not a ton of code, but hard to
 const nav = document.querySelector(
-  "#header,#headerWhite, .kubedb-documentation-menu"
+  "#header,.is-fixed-absolute, #fixedHeader, .kubedb-documentation-menu"
 );
 let topOfNav = nav.offsetTop;
 function fixNav() {
   if (window.scrollY > topOfNav) {
-    document.body.style.paddingTop = nav.offsetHeight + "px";
     document.body.classList.add("fixed-nav");
   } else {
     document.body.classList.remove("fixed-nav");
