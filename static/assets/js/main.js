d331db39e7c8ab25e88d941a5e0a4638e025355d
MD MOHIN <45921824+csemohin@users.noreply.github.com>
2020-02-06 09:51:26 +0600

    if (pre nexSibling p) add margin top 20px, (#65)
    * if (pre > p) add margin top 20px,
    
    Signed-off-by: mohincse <mohin@appscode.com>
    
    * ul>li>pre tag marginTop add
    
    Signed-off-by: mohincse <mohin@appscode.com>
    
    * Fix appscode > home-page > service-section >  consulting-icon path
    
    * target blank attr add in pricing.html page
    
    Signed-off-by: mohincse <mohin@appscode.com>

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,30 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  //For OnScroll Animation JS 
+  // for responsive navbar
+  responsiveNavbar();
+
+  // after preTag conditionally us margin-top
+  marginTopAfterPreTag();
+
+  //For OnScroll Animation JS
   AOS.init({
     once: true
   });
 
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(
-    document.querySelectorAll(".navbar-burger"),
-    0
-  );
+  //Left sidebar Menu fontSize
+  lefSidebarFirstElementFontSize();
 
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-    // Add a click event on each of them
-    $navbarBurgers.forEach(el => {
-      el.addEventListener("click", () => {
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
+  // docs page custom anchor tag
+  docsPageCustomAnchorTag();
 
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle("is-active");
-        $target.classList.toggle("is-active");
-      });
-    });
-  }
+  // docs page right sidebar conditionaly style
+  docsPageRightSidebarConditionalStyle();
 });
 
 // mega menu active class
@@ -172,69 +165,6 @@ const spyScrolling = () => {
 goToASectionSmoothly();
 spyScrolling();
 
-// docs page left sidebar first item font-size
-document.addEventListener("DOMContentLoaded", () => {
-  // left sidebar menu fontSize
-  const sidebarMenu = document.querySelector(".kd-sidebar-menu");
-  if(sidebarMenu){
-    sidebarMenu.children[0].children[1].children[0].style.fontSize = "22px";
-  }
-  
-  const allHeaders = document.querySelectorAll(
-    ".full-info > h2,.full-info > h3,.full-info > h4"
-  );
-   // docs-page -> right sidebar (content > 20) then show a scroll 
-  if((allHeaders.length) > 20){
-    let rightSidebarArea =  document.querySelector('.right-sidebar-area');
-    rightSidebarArea.style.position = 'inherit'
-  }
-  // docs page header link create
-  Array.from(allHeaders).forEach(el => {
-    const id = el.id;
-    const anchorTag = document.createElement("a");
-    anchorTag.className = 'hash-tag'
-    anchorTag.setAttribute("href", "#" + id);
-    anchorTag.innerHTML = `<svg width="25" height="13" viewBox="0 0 52.965 52.973">
-    <g id="broken-link" transform="translate(-0.004)">
-      <path id="Path_16124" data-name="Path 16124" d="M49.467,3.51a12.011,12.011,0,0,0-16.97,0L23.305,12.7a1,1,0,0,0,1.414,1.414l9.192-9.192A10,10,0,0,1,48.052,19.066L36.033,31.088a10.014,10.014,0,0,1-14.143,0A1,1,0,0,0,20.476,32.5a12.013,12.013,0,0,0,16.97,0L49.467,20.48a12.03,12.03,0,0,0,0-16.97Z" fill="#4a4a4a"/>
-      <path id="Path_16125" data-name="Path 16125" d="M26.84,40.279l-7.778,7.778A10,10,0,1,1,4.92,33.915L16.234,22.6a10.015,10.015,0,0,1,14.142,0,1,1,0,0,0,1.414-1.414,12.011,12.011,0,0,0-16.97,0L3.505,32.5A11.987,11.987,0,0,0,11.99,52.973a11.911,11.911,0,0,0,8.485-3.5l7.778-7.778a1,1,0,1,0-1.413-1.414Z" fill="#4a4a4a"/>
-      <path id="Path_16126" data-name="Path 16126" d="M33.969,44.009a1,1,0,0,0-1,1v6a1,1,0,0,0,2,0v-6A1,1,0,0,0,33.969,44.009Z" fill="#4a4a4a"/>
-      <path id="Path_16127" data-name="Path 16127" d="M38.433,42.3a1,1,0,0,0-1.414,1.414l4.243,4.242a1,1,0,0,0,1.414-1.414Z" fill="#4a4a4a"/>
-      <path id="Path_16128" data-name="Path 16128" d="M44.969,38.009h-6a1,1,0,0,0,0,2h6a1,1,0,0,0,0-2Z" fill="#4a4a4a"/>
-      <path id="Path_16129" data-name="Path 16129" d="M15.969,11.009a1,1,0,0,0,1-1v-6a1,1,0,1,0-2,0v6A1,1,0,0,0,15.969,11.009Z" fill="#4a4a4a"/>
-      <path id="Path_16130" data-name="Path 16130" d="M11.5,12.716A1,1,0,0,0,12.918,11.3L8.676,7.06A1,1,0,0,0,7.262,8.474Z" fill="#4a4a4a"/>
-      <path id="Path_16131" data-name="Path 16131" d="M4.969,17.009h6a1,1,0,0,0,0-2h-6a1,1,0,0,0,0,2Z" fill="#4a4a4a"/>
-    </g>
-   </svg>`;
-    el.appendChild(anchorTag);
-
-    //insert hash tag when click anchorTag
-    anchorTag.addEventListener("click", e => {
-      e.preventDefault();
-      const targetEl = document.querySelector(e.currentTarget.hash);
-      window.history.pushState(id, "title", "#" + id);
-      const pos1 = targetEl.offsetTop - 35;
-      window.scrollTo({
-        top: pos1,
-        behavior: "smooth"
-      });
-    });
-  });
-
-  //docs page heading content on reload
-  setTimeout(function() {
-    let getHash = location.hash;
-    if (getHash) {
-      const targetE2 = document.querySelector(getHash);
-      const pos2 = targetE2.offsetTop - 35;
-      scrollTo({
-        top: pos2,
-        behavior: "smooth"
-      });
-    }
-  }, 0);
-});
-
 // tabs active class add script - setup | install page
 const tabItems = document.querySelectorAll(".nav-item .nav-link");
 tabItems.forEach(tab => {
@@ -287,8 +217,10 @@ Array.from(codeHeading).forEach(heading => {
   } else {
     fileType = "txt";
   }
-  let fileName = heading.querySelector('.code-title > h4').textContent.replace(" ", "_");
-  
+  let fileName = heading
+    .querySelector(".code-title > h4")
+    .textContent.replace(" ", "_");
+
   // download js //
   var downloadBtn = heading.querySelector(".download-here");
   if (downloadBtn) {
@@ -306,7 +238,6 @@ Array.from(codeHeading).forEach(heading => {
     });
   }
 });
-
 
 // scroll to top
 var basicScrollTop = function() {
@@ -337,13 +268,109 @@ var basicScrollTop = function() {
 };
 basicScrollTop();
 
+// for responsive navbar
+function responsiveNavbar() {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener("click", () => {
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
 
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
+}
 
+// documentation page (.full-info ol li pre) tag after contain p tag we use margin buttom
+function marginTopAfterPreTag() {
+  let preTag = document.querySelectorAll(".full-info ol li pre, .full-info ul li pre");
+  let pTag = document.querySelector(".full-info ol li p,.full-info ul li p");
+  preTag.forEach(pre => {
+    if ((pre.nextSibling && pre.nextSibling.tagName) == pTag.tagName) {
+      pre.nextSibling.style.marginTop = "20px";
+    }
+  });
+}
 
-// docs page animation code 
-// let allContent = document.querySelectorAll('.full-info > h1,.full-info > h2,.full-info > h3 ,.full-info > h4,.full-info > h5,.full-info > h6, .full-info > ul, .full-info > ol, .full-info > p, .full-info > pre ');
-// Array.from(allContent).forEach(singleContent => {
-//   singleContent.setAttribute('data-aos','fade-up')
-// })
+// left sidebar menu fontSize
+function lefSidebarFirstElementFontSize() {
+  const sidebarMenu = document.querySelector(".kd-sidebar-menu");
+  if (sidebarMenu) {
+    sidebarMenu.children[0].children[1].children[0].style.fontSize = "22px";
+  }
+}
 
+//Docs page headerElement custom anchor tag add
+function docsPageCustomAnchorTag() {
+  // docs page header link create
+  const allHeaders = document.querySelectorAll(
+    ".full-info > h2,.full-info > h3,.full-info > h4"
+  );
+  Array.from(allHeaders).forEach(el => {
+    const id = el.id;
+    const anchorTag = document.createElement("a");
+    anchorTag.className = "hash-tag";
+    anchorTag.setAttribute("href", "#" + id);
+    anchorTag.innerHTML = `<svg width="25" height="13" viewBox="0 0 52.965 52.973">
+      <g id="broken-link" transform="translate(-0.004)">
+        <path id="Path_16124" data-name="Path 16124" d="M49.467,3.51a12.011,12.011,0,0,0-16.97,0L23.305,12.7a1,1,0,0,0,1.414,1.414l9.192-9.192A10,10,0,0,1,48.052,19.066L36.033,31.088a10.014,10.014,0,0,1-14.143,0A1,1,0,0,0,20.476,32.5a12.013,12.013,0,0,0,16.97,0L49.467,20.48a12.03,12.03,0,0,0,0-16.97Z" fill="#4a4a4a"/>
+        <path id="Path_16125" data-name="Path 16125" d="M26.84,40.279l-7.778,7.778A10,10,0,1,1,4.92,33.915L16.234,22.6a10.015,10.015,0,0,1,14.142,0,1,1,0,0,0,1.414-1.414,12.011,12.011,0,0,0-16.97,0L3.505,32.5A11.987,11.987,0,0,0,11.99,52.973a11.911,11.911,0,0,0,8.485-3.5l7.778-7.778a1,1,0,1,0-1.413-1.414Z" fill="#4a4a4a"/>
+        <path id="Path_16126" data-name="Path 16126" d="M33.969,44.009a1,1,0,0,0-1,1v6a1,1,0,0,0,2,0v-6A1,1,0,0,0,33.969,44.009Z" fill="#4a4a4a"/>
+        <path id="Path_16127" data-name="Path 16127" d="M38.433,42.3a1,1,0,0,0-1.414,1.414l4.243,4.242a1,1,0,0,0,1.414-1.414Z" fill="#4a4a4a"/>
+        <path id="Path_16128" data-name="Path 16128" d="M44.969,38.009h-6a1,1,0,0,0,0,2h6a1,1,0,0,0,0-2Z" fill="#4a4a4a"/>
+        <path id="Path_16129" data-name="Path 16129" d="M15.969,11.009a1,1,0,0,0,1-1v-6a1,1,0,1,0-2,0v6A1,1,0,0,0,15.969,11.009Z" fill="#4a4a4a"/>
+        <path id="Path_16130" data-name="Path 16130" d="M11.5,12.716A1,1,0,0,0,12.918,11.3L8.676,7.06A1,1,0,0,0,7.262,8.474Z" fill="#4a4a4a"/>
+        <path id="Path_16131" data-name="Path 16131" d="M4.969,17.009h6a1,1,0,0,0,0-2h-6a1,1,0,0,0,0,2Z" fill="#4a4a4a"/>
+      </g>
+     </svg>`;
+    el.appendChild(anchorTag);
 
+    //insert hash tag when click anchorTag
+    anchorTag.addEventListener("click", e => {
+      e.preventDefault();
+      const targetEl = document.querySelector(e.currentTarget.hash);
+      window.history.pushState(id, "title", "#" + id);
+      const pos1 = targetEl.offsetTop - 35;
+      window.scrollTo({
+        top: pos1,
+        behavior: "smooth"
+      });
+    });
+  });
+
+  //docs page heading content on reload
+  setTimeout(function() {
+    let getHash = location.hash;
+    if (getHash) {
+      const targetE2 = document.querySelector(getHash);
+      const pos2 = targetE2.offsetTop - 35;
+      scrollTo({
+        top: pos2,
+        behavior: "smooth"
+      });
+    }
+  }, 0);
+}
+
+// docs page right sidebar conditionaly style
+function docsPageRightSidebarConditionalStyle() {
+  let allHeaders = document.querySelectorAll(
+    ".full-info > h2,.full-info > h3,.full-info > h4"
+  );
+  // docs-page -> right sidebar (content > 20) then show a scroll
+  if (allHeaders.length > 20) {
+    let rightSidebarArea = document.querySelector(".right-sidebar-area");
+    rightSidebarArea.style.position = "inherit";
+  }
+}
